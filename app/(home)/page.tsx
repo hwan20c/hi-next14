c154499e543d699e50ef63484ea690e3d3cbffc1
hwan20c <hwan20c@gmail.com>
2024-03-25 23:23:01 +0900

    fetch at serverSide

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,17 @@
-"use client";
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-// export const metadata = {
-//   title: "Home",
-// };
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
 
-export default function Tomato() {
-  const [isLoading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const reponse = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await reponse.json();
-    setMovies(json);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
