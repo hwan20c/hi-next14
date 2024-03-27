2d4fa21057b31edf655a85127fa21546b7808bcc
hwan20c <hwan20c@gmail.com>
2024-03-27 23:56:14 +0900

    update with Suspense

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,27 +1,21 @@
-import { API_URL } from "../../../(home)/page";
-
-async function getMovie(id: string) {
-  console.log(`Fetching movies : ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getvideos(id: string) {
-  console.log(`Fetching movies : ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../compnents/movie-info";
+import MovieVideos from "../../../../compnents/movie-videos";
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  console.log("start fetching");
-  // 따로 따로 결과값을 받게 해서 비동기 적으로 수행할 수 있게 만들어주는것이다. -> 그렇지 않으면 앞에 fetching을 기다려야 하기 때문에 앞에가 끝나지 않으면 뒤에 fetching이 진행이 안되기 때문이다.
-  const [movie, viedos] = await Promise.all([getMovie(id), getvideos(id)]);
-  console.log("end fetching");
-  return <h1>{movie.title}</h1>;
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
