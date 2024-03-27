685596043f0998142ca528b2782841499f43571e
hwan20c <hwan20c@gmail.com>
2024-03-28 00:13:55 +0900

    update a error page at a detail movie page

--- compnents/movie-videos.tsx
+++ compnents/movie-videos.tsx
@@ -1,7 +1,8 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  // throw new Error("somting broke...");
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
