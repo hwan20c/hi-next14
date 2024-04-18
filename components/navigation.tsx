9abff7a2503c68be03f7a3bbec940d1d005173fc
hwan20c <hwan20c@gmail.com>
2024-04-18 22:26:37 +0900

    create : back button

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,22 +1,34 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.back(); // 이전 페이지로 이동
+  };
+
   const path = usePathname();
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li>
-          <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
-        </li>
-        <li>
-          <Link href="/about-us">About Us</Link>{" "}
-          {path === "/about-us" ? "🔥" : ""}
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.navWrapper}>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
+          </li>
+          <li>
+            <Link href="/about-us">About Us</Link>{" "}
+            {path === "/about-us" ? "🔥" : ""}
+          </li>
+        </ul>
+      </nav>
+
+      <button className={styles.backButton} onClick={handleBackClick}>
+        ⬅️ Back
+      </button>
+    </div>
   );
 }
